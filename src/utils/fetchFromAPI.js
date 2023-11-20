5f75e0a9be4fb5437ee855c6275fef84f7921498
arulmurugan <arulmurugan6102k.official@gmail..com>
2023-11-20 08:45:35 +0530

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'efd3ee7088msh7bdd938df2337bep180b99jsnab57306285ac',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+ 
+};
